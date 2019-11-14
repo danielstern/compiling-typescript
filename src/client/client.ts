b897388b339fab81e6b3be4ceeb154f4d68cf36d
Daniel Stern <daniel@danielstern.ca>
2019-11-14 09:47:22 -0500

    [WIP]

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,12 +1,21 @@
 import { Question } from '../@types/question';
 
-console.log("Client code executed");
-
 (()=>{
 
     let questions : Question[] = [];
 
-    document.getElementById('QuestionForm').addEventListener("submit", async ()=>{
+    (async function init(){
+
+        const request = await fetch("/questions");
+        questions = await request.json();
+        /* debugger; */
+        /* Remove comment to trigger breakpoint */
+        render();
+
+    })();
+
+
+    const handleSubmitQuestionForm = async () => {
 
         event.preventDefault();
 
@@ -19,45 +28,37 @@ console.log("Client code executed");
         questions = json.questions;
         render();
 
-    });
-
-    function render() {
-        
-        document.getElementById("Questions").innerHTML = questions.map(({title, content, answerCount = 0}) => `
-<li class="list-group-item d-flex justify-content-between lh-condensed">
-
-    <div>
-
-        <h6 class="my-0">
-            
-            ${title}
-        
-        </h6>
-
-        <small class="text-muted">
-            
-            ${content}
-        
-        </small>
-
-    </div>
-
-    <span class="text-muted">
-        
-        ${answerCount} Answer${answerCount === 0 || answerCount > 1 ? `s` : ``}
-
-    </span>
-</li>
-`).join("");
     }
 
-    (async function init(){
+    document.getElementById('QuestionForm').addEventListener("submit", handleSubmitQuestionForm);
 
-        const request = await fetch("/questions");
-        questions = await request.json();
-        debugger;
-        render();
+    const render = () => {
+        
+        document.getElementById("Questions").innerHTML = questions.map(({title, content, answerCount = 0}) => `
+        <li class="list-group-item d-flex justify-content-between lh-condensed">
 
-    })();
+            <div>
 
+                <h6 class="my-0">
+                    
+                    ${title}
+                
+                </h6>
+
+                <small class="text-muted">
+                    
+                    ${content}
+                
+                </small>
+
+            </div>
+
+            <span class="text-muted">
+                
+                ${answerCount} Answer${answerCount === 0 || answerCount > 1 ? `s` : ``}
+
+            </span>
+        </li>
+    `).join("");
+    }
 })();
--- src/server/README.md
+++ src/server/README.md
@@ -2,3 +2,6 @@
 
 ## About
 The server is the back-end component of the application which is never served to the user. It validates information and serves HTML and JavaScript files to the end user.
+
+### Functionality
+The server sends relevant files to browsers accessing the app at the relevant port (`1337` by default). The server also contains a simple REST API that retrieves and adds new questions.
